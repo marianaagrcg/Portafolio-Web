2252247bb79a868d9d563a556b95792f1e00a1aa
marianaagrcg <marianagarciagomez2@gmail.com>
2024-09-11 08:38:46 -0700

    Changes background image

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,6 +2,7 @@
 
 // import { assertAccessor } from "@babel/types";
 import BackgroundImage from "../assets/images/background2.jpeg"
+import Back from '../assets/images/background2.jpeg'
 
 interface Props {}
 
@@ -10,7 +11,7 @@ const Welcome = (_props: Props) => {
         <>
             <section id="Welcome" className="hero is-fullheight-with-navbar bg-fixed" 
             style={{ 
-                backgroundImage: BackgroundImage, 
+                backgroundImage: Back, 
                 backgroundSize: 'cover', 
                 backgroundPosition: 'center' ,
                 opacity: '0.7'
